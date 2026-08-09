035025b5f02b84d77a4e2ef245158e5c67808d67
stuartxthomson <stuartt@gmail.com>
2026-08-09 14:34:05 -0400

    Update sources.py

--- sources.py
+++ sources.py
@@ -2,5 +2,6 @@ FEEDS = {
     "Global News": "https://globalnews.ca/feed/",
     "Politico": "https://rss.politico.com/politics-news.xml",
     "National Post": "https://nationalpost.com/feed/",
-    "Globe and Mail": "https://www.theglobeandmail.com/arc/outboundfeeds/rss/category/politics/"
+    "Globe and Mail": "https://www.theglobeandmail.com/arc/outboundfeeds/rss/category/politics/",
+    "Toronto Star": "https://www.thestar.com/search/?f=rss&t=article&c=politics"
 }
